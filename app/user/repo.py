805133c17ace4bab824c612f0d65f919751ca8b1
Light <danil.odinzov181@gmail.com>
2026-07-21 13:14:31 +0500

    feat: get_with_accounts changed to get_with_accounts_and_payments and get_all_with_accounts to get_all_with_accounts_and_payments

--- app/user/repo.py
+++ app/user/repo.py
@@ -3,6 +3,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
+from app.account.models import Account
 from app.core.constraint_registry import ConstraintRegistry
 from app.core.repo import BaseRepository
 from app.user.models import User
@@ -19,19 +20,20 @@ class UserRepository(BaseRepository[User]):
 
         return result.scalar_one_or_none()
 
-    async def get_with_accounts(self, user_id: UUID) -> User | None:
+
+    async def get_with_accounts_and_payments(self, user_id: UUID) -> User | None:
         result = await self.session.execute(
             select(User)
-            .options(joinedload(User.accounts))
+            .options(joinedload(User.accounts).joinedload(Account.payments))
             .where(User.id == user_id)
         )
 
-        return result.unique().scalar_one_or_none()
+        return result.scalar_one_or_none()
 
-    async def get_all_with_accounts(self) -> list[User]:
+    async def get_all_with_accounts_and_payments(self) -> list[User]:
         result = await self.session.execute(
             select(User)
-            .options(joinedload(User.accounts))
+            .options(joinedload(User.accounts).joinedload(Account.payments))
             .order_by(User.id)
         )
 
