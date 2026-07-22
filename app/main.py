f20f65fa0027b43004b5c67b8273bedee1425706
Light <danil.odinzov181@gmail.com>
2026-07-22 16:06:17 +0500

    feat: add actual main.py

--- app/main.py
+++ app/main.py
@@ -1,36 +1,33 @@
-from contextlib import asynccontextmanager
+import logging
 
-from fastapi import FastAPI
+from sanic import Sanic
 
-from app.core.config import settings
-from app.core.database import Database
-
-
-@asynccontextmanager
-async def lifespan(
-    application: FastAPI,
-):
-    database = None
-
-    try:
-        database = Database(
-            config=settings.db_settings,
-        )
-
-        await database.health_check()
-
-        application.state.database = database
-
-        yield
-
-    except Exception:
-        raise
-
-    finally:
-        if database is not None:
-            await database.dispose()
+from app.auth.routers import auth_bp
+from app.core.deps import setup_dependencies
+from app.core.listeners import setup_listeners
+from app.core.logger import setup_logger
+from app.core.middleware import setup_middlewares
+from app.core.scheduler import init_scheduler
+from app.user.routers import user_bp
 
 
-app = FastAPI(
-    lifespan=lifespan,
-)
+logger = logging.getLogger(__name__)
+
+def create_app() -> Sanic:
+    app = Sanic("FinCore")
+
+    setup_logger()
+
+    app.blueprint(auth_bp)
+    app.blueprint(user_bp)
+
+    setup_middlewares(app)
+    setup_listeners(app)
+    setup_dependencies(app)
+
+    # to expire revoked jwt tokens
+    init_scheduler(app)
+
+    return app
+
+
