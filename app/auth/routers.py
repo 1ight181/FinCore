064c594aad3f16426409995089b25de1f57f3b94
Light <danil.odinzov181@gmail.com>
2026-07-22 22:54:28 +0500

    doc: add openAPI docs for auth

--- app/auth/routers.py
+++ app/auth/routers.py
@@ -1,22 +1,71 @@
-from sanic import Blueprint
-from sanic.response import JSONResponse
+from sanic import Blueprint, json, empty
+from sanic_ext import openapi
+from starlette import status
 
 from app.auth.schemas import LoginRequest, TokenResponse
 from app.auth.services import AuthService
 from app.auth.types import CurrentUser
 
-auth_bp = Blueprint("auth", url_prefix="/auth")
+
+auth_bp = Blueprint(
+    "auth",
+    url_prefix="/auth",
+)
 
 
 @auth_bp.post("/login")
-async def login(request, auth_service: AuthService):
+@openapi.summary("Login user")
+@openapi.description(
+    "Authenticate user by email and password and return JWT access token."
+)
+@openapi.body(
+    {
+        "application/json": LoginRequest
+    },
+)
+@openapi.response(
+    200,
+    {
+        "application/json": TokenResponse
+    },
+    description="Successful authentication"
+)
+@openapi.response(
+    401,
+    description="Invalid credentials"
+)
+async def login(
+    request,
+    auth_service: AuthService,
+):
     data = LoginRequest.model_validate(request.json)
-    token = await auth_service.login(data.email, data.password)
 
-    return JSONResponse(TokenResponse(access_token=token).model_dump())
+    token = await auth_service.login(
+        data.email,
+        data.password,
+    )
+
+    return json(
+        TokenResponse(
+            access_token=token
+        ).model_dump(mode="json",)
+    )
 
 
 @auth_bp.post("/logout")
+@openapi.summary("Logout user")
+@openapi.description(
+    "Invalidate current JWT token."
+)
+@openapi.secured("bearerAuth")
+@openapi.response(
+    200,
+    description="Successfully logged out"
+)
+@openapi.response(
+    401,
+    description="Unauthorized"
+)
 async def logout(
     request,
     _: CurrentUser,
@@ -28,12 +77,9 @@ async def logout(
 
     token = auth_header.split(" ")[1]
 
-    await auth_service.logout(
-        token
+    await auth_service.logout(token)
+
+    return empty(
+        status=status.HTTP_200_OK,
     )
 
-    return JSONResponse(
-        {
-            "status": "logged_out"
-        },
-    )
